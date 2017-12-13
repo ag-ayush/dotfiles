36ceaae3a3a09c1374e359ef78303dbbd8a06e87
ag-ayush <ag.goelayush@gmail.com>
2017-12-13 10:26:14 -0500

    adding script to copy my files for me, updating some polybar stuff

---
+++ copy.sh
@@ -0,0 +1,5 @@
+# Copy files over.
+yes | cp -rf $HOME/.config/polybar $HOME/git/dotfiles/
+
+yes | cp -rf $HOME/.config/i3/config $HOME/git/dotfiles/i3
+
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c /home/agoel/git/dotfiles/polybar/config custom &
+polybar example &
 
 
 echo "Bars launched...
