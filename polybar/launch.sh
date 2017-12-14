286f87122b4438959f35fbad0852979acd10b440
ag-ayush <ag.goelayush@gmail.com>
2017-12-14 18:50:31 -0500

    Updating polybar theme, adding custom lock script, and update to config i3 too

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,10 +4,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar example &
+# Launch top-bar
+polybar -c $HOME/.config/polybar/config2 top &
+echo "Bars launched..."
 
-
-echo "Bars launched...
