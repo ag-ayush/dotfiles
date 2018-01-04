608c010fa4bf1d6e7d89be6fb49033ee088af90e
ag-ayush <ag.goelayush@gmail.com>
2018-01-03 19:20:41 -0500

    small update to install, tells when installing polybar i3 separately

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ echo -e "Done\n"
 
 
 ### polybar and i3 ###
-echo -e "Installing polybar & i3-gaps \n"
+echo -e "Installing polybar & i3-gaps dependencies\n"
 ## Dependencies
 
 # polybar
@@ -78,6 +78,7 @@ sudo apt install libxcb-xrm-dev
 #sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake 
 
 ## Install polybar
+echo -e "Installing polybar \n"
 cd $HOME/git/linux
 # clone the repository
 git clone --recursive https://github.com/jaagr/polybar
@@ -88,6 +89,7 @@ cmake ..
 sudo make install
 
 ## Install i3-gaps
+echo -e "Installing i3-gaps \n"
 cd $HOME/git/linux
 # clone the repository
 git clone https://www.github.com/Airblader/i3 i3-gaps
