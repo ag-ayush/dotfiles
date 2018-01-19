c8726a5261377de8c997bdb8a77a894db9f105a7
ag-ayush <ag.goelayush@gmail.com>
2018-01-19 00:28:12 -0500

    changed up the script, works on 17.10 now

--- t470/install.sh
+++ t470/install.sh
@@ -77,11 +77,11 @@ sudo apt install --fix-broken -q=3
 echo -e "Done\n"
 
 # Ubuntu 16.04 or less
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
+#sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
 
 # Install Packages
 echo -e "Installing apt packages\n"
-install_packages neovim git python3 python3-pip php openjdk-8-* arc-theme npm nodejs nodejs-legacy zsh papirus-icon-theme paper-icon-theme paper-cursor-theme paper-gtk-theme xcb-util-xrm fonts-font-awesome pycharm vlc
+install_packages neovim git python3 python3-pip php openjdk-9-* arc-theme npm nodejs nodejs-legacy zsh papirus-icon-theme paper-icon-theme paper-cursor-theme paper-gtk-theme xcb-util-xrm fonts-font-awesome pycharm vlc
 echo -e "Done\n"
 
 ###############################################################################
@@ -105,9 +105,9 @@ echo -e "Installing polybar & i3-gaps dependencies\n"
 install_packages cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libxcb-cursor-dev libjsoncpp libsigc++
 ## i3
 # Ubuntu 16.04 or less
-install_packages libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf rofi compton blueman-applet nm-applet mdp i3status dmenu libxcb-xrm-dev i3lock feh
+#install_packages libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf rofi compton blueman-applet nm-applet mdp i3status dmenu libxcb-xrm-dev i3lock feh
 # Ubuntu 16.10+
-#install_packages libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake rofi compton blueman-applet nm-applet mdp i3status dmenu libxcb-xrm-dev i3lock feh
+install_packages libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake rofi compton blueman-applet nm-applet mdp i3status dmenu libxcb-xrm-dev i3lock feh
 echo -e "Done\n"
 
 ###############################################################################
@@ -155,6 +155,20 @@ echo -e "Done\n"
 mkdir $HOME/Pictures/Wallpapers
 wget -O $HOME/Pictures/Wallpapers/tardis-abstract.jpg https://www.walldevil.com/wallpapers/a71/wallpaper-images-abstract-tardis-cartoons.jpg
 gsettings set org.gnome.desktop.background picture-uri file:///$HOME/Pictures/Wallpapers/tardis-abstract.jpg
+echo -e "Added and changed wallpaper\n"
+
+###############################################################################
+#
+# IntelliJ
+#
+###############################################################################
+echo -e "installing intelliJ\n"
+cd $HOME/Downloads
+wget https://download.jetbrains.com/idea/ideaIC-2017.3.2.tar.gz
+sudo tar xf ideaIC-2017.3.2.tar.gz -C /opt/
+cd /opt/idea-IC-173.4127.27/bin
+./idea.sh
+echo -e "done installing intelliJ\n"
 
 ###############################################################################
 #
@@ -172,20 +186,7 @@ git clone https://github.com/devinmatte/zsh-syntax-highlighting.git $ZSH_CUSTOM/
 echo -e "Done\n"
 # Switch to zsh as shell
 echo -e "Changing Shell to ZSH\n"
-sudo chsh -s $(which zsh)
-exit
 echo -e "Done\n"
-
-###############################################################################
-#
-# IntelliJ
-#
-###############################################################################
-cd $HOME/Downloads
-wget https://download.jetbrains.com/idea/ideaIC-2017.3.2.tar.gz
-sudo tar xf ideaIC-2017.3.2.tar.gz -C /opt/
-cd /opt/idea-IC-173.4127.27/bin
-./idea.sh
 
 ###############################################################################
 #
@@ -193,3 +194,5 @@ cd /opt/idea-IC-173.4127.27/bin
 #
 ###############################################################################
 echo -e "\n\nDONE DONE DONE DONE DONE DONE DONE DONE DONE DONE DONE DONE\n\n"
+sudo chsh -s $(which zsh)
+exit
