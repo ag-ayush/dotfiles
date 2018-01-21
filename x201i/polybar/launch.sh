50d1878e1f851dd2318b568c463c7240664fb7f6
ag-ayush <ag.goelayush@gmail.com>
2018-01-20 21:46:54 -0500

    adding multiple monitor capability for polybar

--- x201i/polybar/launch.sh
+++ x201i/polybar/launch.sh
@@ -6,7 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch top-bar
-polybar -c $HOME/.config/polybar/config2 top &
-echo "Bars launched..."
-
+# Launch top-bar, depending on monitors launch on all
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload -c $HOME/.config/polybar/config2 top &
+  done
+else
+  polybar --reload -c $HOME/.config/polybar/config2 top &
+fi
