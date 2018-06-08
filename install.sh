b089aa6080976d1576a6011234625a7245f3c2ce
axg4975 <axg4975@rit.edu>
2018-06-08 23:01:47 +0530

    little updates
    changing rofi up and clearing things out
    to make it much more managable.

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 ###############################################################################
 #
-# Location, change based on which computer you are installing to.
-# Some differences: polybar battery and order of items, i3 blueman applet
+# Location
 #
 ###############################################################################
-LOCATION="$HOME/git/linux/dotfiles/t470"
+LOCATION="$HOME/git/linux/dotfiles"
 
 ###############################################################################
 #
@@ -81,7 +80,7 @@ echo -e "Done\n"
 
 # Install Packages
 echo -e "Installing apt pip3 apm packages\n"
-install_packages neovim git python3 python3-pip python-pip php arc-theme npm nodejs nodejs-legacy zsh papirus-icon-theme paper-icon-theme paper-cursor-theme paper-gtk-theme xcb-util-xrm fonts-font-awesome pycharm vlc terminator scrot make texlive pandoc sshfs help2man blueman libfftw3-dev libncursesw5-dev libpulse-dev glances texlive-full lxappearance
+install_packages neovim git python3 python3-pip python-pip php arc-theme npm nodejs nodejs-legacy zsh papirus-icon-theme paper-icon-theme paper-cursor-theme paper-gtk-theme xcb-util-xrm fonts-font-awesome pycharm vlc terminator scrot make texlive pandoc sshfs help2man blueman libfftw3-dev libncursesw5-dev libpulse-dev glances texlive-full lxappearance zathura
 
 sudo pip3 install -r $HOME/git/linux/dotfiles/requirements.txt
 
