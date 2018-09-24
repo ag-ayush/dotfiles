776cd5cd48ebd8b8b55ff5853f56d0de2b5baa17
ag-ayush <ag.goelayush@gmail.com>
2018-09-24 09:02:49 -0400

    Update of small changes over several weeks of use

--- night-city-theme/polybar/launch.sh
+++ night-city-theme/polybar/launch.sh
@@ -7,6 +7,13 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch top-bar
-polybar -c $HOME/.config/polybar/night-config top &
-echo "Bars launched..."
+#polybar -c $HOME/.config/polybar/night-config top &
+#echo "Bars launched..."
 
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar -c $HOME/.config/polybar/night-config top &
+	done
+else
+	polybar -c $HOME/.config/polybar/night-config &
+fi
